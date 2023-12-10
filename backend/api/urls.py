44cea666add892b607322377780902e3534d1a73
controldevil007 <controldevil007@gmail.com>
2023-12-11 02:50:05 +0530

    added token auth for py_client

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
+
 # from .views import api_home
 
-urlpatterns = [
-    path('', views.api_home)
-]
+urlpatterns = [path("auth/", obtain_auth_token), path("", views.api_home)]
--- py_client/create.py
+++ py_client/create.py
@@ -1,13 +1,9 @@
 import requests
 
+headers = {"Authorization": "Bearer fd106f9fa2248f38157d7c5fd8da21f57383d0c8"}
 endpoint = "http://localhost:8000/api/products/"
 
-data = {
-    "title": "This field is done"
-}
+data = {"title": "This field is done"}
 
-get_response = requests.post(endpoint,json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
-
-
-
